d2fa29d032d58e618c2ba33f54ba8212bcd981b4
Gravener <gravener@BAM-024170-RG.mlbam.mlb.org>
2014-09-22 09:54:20 -0400

    ok

--- trainapp/njrails.sh
+++ trainapp/njrails.sh
@@ -1,8 +1,7 @@
-read -s -p "Download new schedule? " -n 1 newschedule
+read -s -p "password " mypassword
 
 yes='y'
 no='n'
-if [ "$yes" == "$newSchedule" ]; then
 	read -s -p "Enter Password for snooplsm: " mypassword
 	echo ""
 	curl -c cookies.txt -o /dev/null -L "http://www.njtransit.com"
@@ -10,7 +9,7 @@ if [ "$yes" == "$newSchedule" ]; then
 	curl -b cookies.txt -o /dev/null -b cookies.txt -L -d "userName=snooplsm&password=$mypassword" "https://www.njtransit.com/mt/mt_servlet.srv?hdnPageAction=MTDevLoginSubmitTo"
 	curl -b cookies.txt -c cookies.txt --dump-header headers.txt -L "https://www.njtransit.com/mt/mt_servlet.srv?hdnPageAction=MTDevResourceDownloadTo&Category=rail" -o njtransit.zip
 	unzip -o njtransit.zip -d gtfs/njtransit/
-fi
+
 cd ..
 file="$PWD/libraries/library/res/raw/database_db"
 cd trainapp
