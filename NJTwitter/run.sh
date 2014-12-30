8e1c365c70423a5c33df953a1cf918f3d1d10a6c
rgravene <ryan.gravener@mlb.com>
2014-12-30 17:10:52 -0500

    add java command

--- NJTwitter/run.sh
+++ NJTwitter/run.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 PWD=${PWD}
 echo $PWD
 mkdir bin
 
 find src -type f -name *.java -exec javac -verbose -cp "libs/*" -sourcepath src -d bin -source 7 -target 7 -encoding utf-8 {} +
-# java -cp "bin:libs/*" us.wmwm.happytap.stream.HappyStream $1 $2 $3 $4 $5 $6 $7 &
+java -cp "bin:libs/*" us.wmwm.happytap.stream.HappyStream $1 $2 $3 $4 $5 $6 $7 &
